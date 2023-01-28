2c3e1c1044d24a438ccd2e2782e631b5a2bb1aca
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-01-27 23:24:05 -0300

    Se quita morgan y se modifica una ruta

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const express=require('express');
 const app=express()
-const morgan=require('morgan');
 const cors=require('cors');
 const indexRoute=require('./routes/indexRouter')
 const db=require('./model/db');
 db.conectar();
 
-
-app.use(morgan('dev'));
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/api/huellitas', indexRoute);
+app.use('/api', indexRoute);
 app.use((req,res,next)=>{
     res.status(404).send('El recurso no existe');
 });
